fc049589f88ba84671139be9619d1b467910a1c9
dabasishJoy <dabasish@6sensehq.com>
2023-04-10 13:25:32 +0600

    added comments module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DeveloperModule } from './developer/developer.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DB_CONNECTION_STRING),
     DeveloperModule,
+    CommentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
