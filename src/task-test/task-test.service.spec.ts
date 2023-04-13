31e2b96054d5f2a214dd921cf6c4c68a0ff6ef41
dabasishJoy <dabasish@6sensehq.com>
2023-04-13 12:46:40 +0600

    unit testing of functions

--- src/task-test/task-test.service.spec.ts
+++ src/task-test/task-test.service.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TaskTestService } from './task-test.service';
 
@@ -12,11 +13,138 @@ describe('TaskTestService', () => {
     service = module.get<TaskTestService>(TaskTestService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('fibonacci series', () => {
+    it('should be defined', () => {
+      expect(service).toBeDefined();
+    });
+
+    it('should return a fibonacci number of a given number', async () => {
+      // arrange
+      const number = 5;
+
+      // act
+      const fibonacci = await service.calculateFibonacci(number);
+
+      // assert
+      expect(fibonacci).toBe(5);
+    });
+
+    it('Should throw exception with number equal to 0', async () => {
+      // arrage
+      const number = 0;
+
+      // actw
+      const res = () => {
+        return service.calculateFibonacci(number);
+      };
+      // assert
+
+      await expect(res).rejects.toBeInstanceOf(BadRequestException);
+    });
+
+    it('Should throw exception with number less than 0', async () => {
+      // arrage
+      const number = -5;
+
+      // actw
+      const res = () => {
+        return service.calculateFibonacci(number);
+      };
+      // assert
+
+      await expect(res).rejects.toBeInstanceOf(BadRequestException);
+    });
   });
 
-  it('should return a fibonacci number of a given number', async () => {
-    expect(await service.calculateFibonacci(5)).toEqual(5);
+  describe('Merge Sort', () => {
+    it('Should return the sorted array', async () => {
+      // arrange
+      const nums: number[] = [4, 0, 5, 2, 9];
+
+      // act
+      const result = await service.mergeSort(nums);
+
+      // assert
+      expect(result).toEqual([0, 2, 4, 5, 9]);
+    });
+
+    it('Should return the given array if the array length is 1', async () => {
+      // arrange
+      const nums: number[] = [4];
+
+      // act
+      const result = await service.mergeSort(nums);
+
+      // assert
+      expect(result).toEqual([4]);
+    });
+
+    it('Should return the given array if the array length is less that 1', async () => {
+      // arrange
+      const nums: number[] = [];
+
+      // act
+      const result = await service.mergeSort(nums);
+
+      // assert
+      expect(result).toEqual([]);
+    });
+
+    // merge function
+    it('Should return the merged array if two array are given', async () => {
+      // arrange
+      const leftArr: number[] = [4, 7, 10];
+      const rightArr: number[] = [1, 3, 8];
+
+      // act
+      const result = await service.merge(leftArr, rightArr);
+
+      // assert
+      expect(result).toEqual([1, 3, 4, 7, 8, 10]);
+    });
+  });
+
+  describe('factorial', () => {
+    it('should return the factorial of a number', () => {
+      const number = 5;
+
+      const factrorial = service.factorial(number);
+
+      expect(factrorial).toEqual(120);
+    });
+
+    it('should return 1 if the number is 1', () => {
+      const number = 1;
+
+      const factrorial = service.factorial(number);
+
+      expect(factrorial).toEqual(1);
+    });
+
+    it('should return 0 if the number is 1', () => {
+      const number = 0;
+
+      const factrorial = service.factorial(number);
+
+      expect(factrorial).toEqual(1);
+    });
+  });
+
+  describe('ractangeArea', () => {
+    it('should return the area of rectangle', () => {
+      expect(service.rectangleArea(0, 0)).toEqual(0);
+    });
+
+    it('should return thow an error if width or height is negative', () => {
+      expect(() => service.rectangleArea(-2, 5)).toThrow(
+        'Width and height must be non-negative.',
+      );
+    });
+
+    it('should return thow an error if width or height is negative', () => {
+      expect(() => service.rectangleArea(5, -3)).toThrow(
+        'Width and height must be non-negative.',
+      );
+    });
   });
 });
--- src/task-test/task-test.service.ts
+++ src/task-test/task-test.service.ts
@@ -17,4 +17,58 @@ export class TaskTestService {
     }
     return curr;
   }
+
+  //   async addition(n1: number, n2: number): Promise<number> {
+  //     if(n1)
+  //   }
+
+  //   merge sort
+  async mergeSort(arr: number[]): Promise<number[]> {
+    if (arr.length <= 1) {
+      return arr;
+    }
+    const mid = Math.floor(arr.length / 2);
+    const leftArr = arr.slice(0, mid);
+    const rightArr = arr.slice(mid);
+    const leftSorted = await this.mergeSort(leftArr);
+    const rightSorted = await this.mergeSort(rightArr);
+    return await this.merge(leftSorted, rightSorted);
+  }
+
+  //   merge part
+  async merge(leftArr: number[], rightArr: number[]): Promise<number[]> {
+    const mergedArr: number[] = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+      if (leftArr[leftIndex] < rightArr[rightIndex]) {
+        mergedArr.push(leftArr[leftIndex++]);
+      } else {
+        mergedArr.push(rightArr[rightIndex++]);
+      }
+    }
+    return mergedArr
+      .concat(leftArr.slice(leftIndex))
+      .concat(rightArr.slice(rightIndex));
+  }
+
+  //   factorial
+  factorial(n: number): number {
+    // base case of 0 or 1
+    if (n === 0 || n === 1) {
+      return 1;
+    }
+
+    // Recursively calculate the factorial for n-1
+    return n * this.factorial(n - 1);
+  }
+
+  rectangleArea(width: number, height: number): number {
+    // Handle the case of negative inputs
+    if (width < 0 || height < 0) {
+      throw new Error('Width and height must be non-negative.');
+    }
+
+    return width * height;
+  }
 }
